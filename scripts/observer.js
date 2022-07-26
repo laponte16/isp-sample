06763d16ac10dc1c54c4a8ca730515d7aed5c048
laponte16 <54826992+laponte16@users.noreply.github.com>
2022-07-26 16:34:15 -0400

    Observer listo

--- scripts/observer.js
+++ scripts/observer.js
@@ -1,18 +1,19 @@
 const observer = new IntersectionObserver(entries => {
-    // Loop over the entries
     entries.forEach(entry => {
-      // If the element is visible
-      if (entry.isIntersecting) {
-        // Add the animation class
-        entry.target.classList.add('homeAreas2TextA');
-        
-      }
-      else{
-        entry.target.classList.remove('homeAreas2TextA');
-      }
-      
+        /*Aqui se especifica quien es el blanco de interes*/
+        if(entry.target.classList.contains('homeAreas2Text') === true)
+        {
+            /*Aqui se pregunta si el blanco esta interceptando la vista*/
+            if (entry.isIntersecting) {
+                entry.target.classList.add('homeAreas2TextA');  
+              }
+              /*Aqui se elimina la clase agregada al salir de la vista*/ 
+              else{
+                entry.target.classList.remove('homeAreas2TextA');
+              }
+        }     
     });
   });
-  
-  observer.observe(document.querySelector('.homeAreas2Text1'));
-  observer.observe(document.querySelector('.homeAreas2Text2'));
+  /*Aqui se elige quienes son los elementos a observar. En este caso, hay dos con el mismo nombre por simplicidad, tomados como un array. De lo contrario, se pudo usar querySelector solamente, el cual retorna el primer elemento con el nombre pedido*/
+  observer.observe(document.querySelectorAll('.homeAreas2Text')[0]);
+  observer.observe(document.querySelectorAll('.homeAreas2Text')[1]);
