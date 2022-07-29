ba4dc5e9da4aae6cf858933e100edae7c9353796
laponte16 <54826992+laponte16@users.noreply.github.com>
2022-07-29 15:46:29 -0400

    bobbing

--- scripts/observer.js
+++ scripts/observer.js
@@ -16,11 +16,22 @@ const observer = new IntersectionObserver(entries => {
         {
             /*Aqui se pregunta si el blanco esta interceptando la vista*/
             if (entry.isIntersecting) {
-                entry.target.classList.add('bob-side');  
+              entry.target.classList.add('bob-side');
               }
               /*Aqui se elimina la clase agregada al salir de la vista*/ 
               else{
                 entry.target.classList.remove('bob-side');
+              }
+        }
+        if(entry.target.classList.contains('about2') === true)
+        {
+            /*Aqui se pregunta si el blanco esta interceptando la vista*/
+            if (entry.isIntersecting) {
+              entry.target.classList.add('bob-side2');
+              }
+              /*Aqui se elimina la clase agregada al salir de la vista*/ 
+              else{
+                entry.target.classList.remove('bob-side2');
               }
         }    
     });
@@ -28,6 +39,7 @@ const observer = new IntersectionObserver(entries => {
   /*Aqui se elige quienes son los elementos a observar. En este caso, se uso un forEach para tomar todos los que existan, tomados como un array. De lo contrario, se pudo usar querySelector solamente, el cual retorna el primer elemento con el nombre pedido*/
   document.querySelectorAll('.shaky').forEach(
     Element => observer.observe(Element));
-    document.querySelectorAll('.about').forEach(
-      Element => observer.observe(Element));
-
+  document.querySelectorAll('.about').forEach(
+    Element => observer.observe(Element));
+  document.querySelectorAll('.about2').forEach(
+    Element => observer.observe(Element));
