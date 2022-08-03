0b7f5c8a97b7d171b4bfee83c99cb7e894ad0994
laponte16 <54826992+laponte16@users.noreply.github.com>
2022-08-03 18:02:17 -0400

    Update conexion.php

--- en/back-end/conexion.php
+++ en/back-end/conexion.php
@@ -1,5 +1,6 @@
 <?php 
-	$link = mysqli_connect("localhost","root","","isp_sample");
+	$link = mysqli_connect("ec2-107-22-122-106.compute-1.amazonaws.com","blmpcpjcxstyva","7a715b95d910d73e924e34f4074ee9d0583d6f6416f1a296e01d933999162f15
+    ","d7h894o3lgdppj");
 	if (!$link) 
     {
     die("Connection failed: " . mysqli_connect_error());
